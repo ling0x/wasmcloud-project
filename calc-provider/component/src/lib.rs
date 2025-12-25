97d6a8928a13ba2b52b5206ece40650eedb37a49
ling0x <ling0x@users.noreply.github.com>
2025-12-25 22:09:07 +0000

    feat: custom provider

--- calc-provider/component/src/lib.rs
+++ calc-provider/component/src/lib.rs
@@ -3,17 +3,18 @@ wit_bindgen::generate!({ generate_all });
 use crate::exports::wasmcloud::example::process_data::Data;
 use crate::exports::wasmcloud::example::process_data::Guest;
 use crate::wasi::logging::logging::*;
-use crate::wasmcloud::example::system_info::Kind;
+use crate::wasmcloud::example::calculator::Numbers;
 
 struct CustomTemplateComponent;
 
 impl Guest for CustomTemplateComponent {
     fn process(data: Data) -> String {
         log(Level::Info, "", &format!("Data received: {:?}", data));
-        // Request OS and architecture information
-        let os = crate::wasmcloud::example::system_info::request_info(Kind::Os);
-        let arch = crate::wasmcloud::example::system_info::request_info(Kind::Arch);
-        format!("Provider is running on the operating system: {os}-{arch}").to_string()
+        let result = crate::wasmcloud::example::calculator::calculate(Numbers {
+            number_one: data.first_number,
+            number_two: data.second_number,
+        });
+        format!("Provider is calculating {result:#?}").to_string()
     }
 }
 
